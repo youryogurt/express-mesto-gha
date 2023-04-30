116905629a9aeb653278b99fe3d19ea23e32eb55
Janna Gurtovaia <zhanna.gurt@gmail.com>
2023-04-30 18:09:47 +0300

    fix errors part 8

--- controllers/cards.js
+++ controllers/cards.js
@@ -22,25 +22,6 @@ const createCard = (req, res) => {
     });
 };
 
-// const deleteCardById = async (req, res) => {
-//   const { cardId } = req.params;
-//   if (!cardId || !mongoose.Types.ObjectId.isValid(cardId)) {
-//     res.status(400).json({ message: 'Некорректный id карточки' });
-//     return;
-//   }
-
-//   try {
-//     const deletedCard = await Card.findByIdAndDelete(cardId);
-//     if (!deletedCard) {
-//       res.status(404).json({ message: 'Карточка с указанным _id не найдена' });
-//     } else {
-//       res.json(deletedCard);
-//     }
-//   } catch (err) {
-//     res.status(500).json({ message: err.message });
-//   }
-// };
-
 const deleteCardById = (req, res) => {
   const { cardId } = req.params;
   if (mongoose.Types.ObjectId.isValid(cardId)) {
@@ -59,24 +40,6 @@ const deleteCardById = (req, res) => {
     res.status(400).send({ message: 'Некорректный id карточки' });
   }
 };
-
-// const likeCard = async (req, res) => {
-//   try {
-//     const { cardId } = req.params;
-//     const card = await Card.findByIdAndUpdate(
-//       cardId,
-//       { $addToSet: { likes: req.user._id } },
-//       { new: true },
-//     ).populate('owner likes');
-//     if (!card) {
-//       res.status(404).json({ message: 'Передан несуществующий _id карточки' });
-//     } else {
-//       res.status(200).json(card);
-//     }
-//   } catch (err) {
-//     res.status(500).json({ message: err.message });
-//   }
-// };
 
 const likeCard = (req, res) => {
   const { cardId } = req.params;
@@ -101,21 +64,26 @@ const likeCard = (req, res) => {
   }
 };
 
-const dislikeCard = async (req, res) => {
-  try {
-    const { cardId } = req.params;
-    const card = await Card.findByIdAndUpdate(
+const dislikeCard = (req, res) => {
+  const { cardId } = req.params;
+  if (mongoose.Types.ObjectId.isValid(cardId)) {
+    Card.findByIdAndUpdate(
       cardId,
       { $pull: { likes: req.user._id } },
       { new: true },
-    ).populate('owner likes');
-    if (!card) {
-      res.status(404).json({ message: 'Карточка не найдена' });
-    } else {
-      res.status(200).json(card);
-    }
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+    )
+      .then((card) => {
+        if (card) {
+          res.send({ data: card });
+        } else {
+          res.status(404).send({ message: 'Карточка не найдена' });
+        }
+      })
+      .catch((err) => {
+        res.status(500).send({ message: err.message });
+      });
+  } else {
+    res.status(400).send({ message: 'Некорректный id карточки' });
   }
 };
 
