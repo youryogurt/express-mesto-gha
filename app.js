881ec4b4a178bddc29d718fc6acd70cecd859a2d
Janna Gurtovaia <zhanna.gurt@gmail.com>
2023-05-22 19:52:15 +0200

    change mongo address

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.listen(PORT, () => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 })
   .then(() => console.log('MongoDB connected'))
